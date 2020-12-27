703a2a3d2fc9650de5af0e60c085b537ab6ce17b
edgar9880 <edgar.ahmadeev@gmail.com>
2020-12-27 20:17:24 +0300

    lr update

--- 9. LR/lr.py
+++ 9. LR/lr.py
@@ -1,95 +1,66 @@
-import plotly.graph_objects as go
 import numpy as np
-from sklearn.cluster import KMeans
+from plotly import graph_objects
 
 
-def predict(features, weights):
-    zz = np.dot(features, weights)
-    return sigmoid(zz)
+LEARNING_RATE = 0.0001
+ITERATION_COUNT = 1000
 
 
-def sigmoid(zz):
-    num = 1 / (1 + np.exp(-zz))
-    return num
+def get_coordinates(dots):
+    return np.array(list(zip(*dots)))
 
 
-def update_weights(features, labels, weights, lr):
-    N = len(features)
-    predictions = predict(features, weights)
+def gradient(x, y, z):
+    """
+    Градиентный спуск
+    :param x:
+    :param y:
+    :param z:
+    :return: coefficients
+    """
+    a, b, c = 0, 0, 0
+    n = float(len(x))
 
-    gradient = np.dot(np.transpose(features), predictions - labels)
-    gradient /= N
-    gradient *= lr
+    for i in range(ITERATION_COUNT):
+        z_predicted = a * x + b * y + c
+        # (learning rate) * (derivative with respect to *)
+        a -= LEARNING_RATE * ((-1 / n) * sum(x * (z - z_predicted)))
+        b -= LEARNING_RATE * ((-1 / n) * sum(y * (z - z_predicted)))
+        c -= LEARNING_RATE * ((-1 / n) * sum(z - z_predicted))
 
-    weights -= gradient
-    return weights
-
-
-def train(features, labels, weights, lr, iters):
-    for i in range(iters):
-        weights = update_weights(features, labels, weights, lr)
-
-    return weights
-
-
-def decision(procent):
-    return 'blue' if procent >= 0.5 else 'red'
+    return a, b, c
 
 
-def log(weighss, points):
-    print("A: ", weighss[0], "  B: ", weighss[1], "  C: ", weighss[2], )
-    print("Новая точка:", points[(len(points) - 1)])
-    print("Предсказание новой точки:", decision(predict(points, weighss)[(len(points) - 1)]),
-          "% =: ", '{:0.9f}'.format(predict(points, weighss)[(len(points) - 1)] * 100))
+def create_random_dots(min_v=0, max_v=100, count=100, d=3):
+    """
+    Создаем рандомные точки
+    :param min_v: int
+    :param max_v: int
+    :param count: int
+    :param d: Размерность int
+    :return: list of tuples (x, y, z)
+    """
+    dots = []
+    for i in range(count):
+        x, y, z = [np.random.randint(min_v, max_v) for i in range(d)]
+        dots.append((x, y, z))
+    return dots
 
 
-def show_figure(x, y, z, xx, yx, zz, colors):
-    fig = go.Figure()
-    fig.add_trace(go.Scatter3d(x=x, y=y, z=z, mode='markers', marker=dict(color=colors)))
-    fig.add_trace(go.Surface(x=xx, y=yx, z=zz(xx, yx), opacity=0.9, colorscale='Viridis'))
-    fig.show()
+dots = create_random_dots()
+x, y, z = get_coordinates(dots)
+a, b, c = gradient(x, y, z)
 
+tmp = np.linspace(0, 100, 100)
+x_coordinates, y_coorinates = np.meshgrid(tmp, tmp)
 
-if __name__ == '__main__':
-    # Рандомно задаем точки
-    n = 50
+figure = graph_objects.Figure(
+    data=[graph_objects.Scatter3d(x=x, y=y, z=z, mode='markers',)]
+)
+figure.add_trace(
+    graph_objects.Surface(
+        x=x_coordinates, y=y_coorinates, z=a * x_coordinates + b * y_coorinates + c
+    )
+)
+figure.show()
 
-    # coordinates (3d)
-    x = np.random.randint(0, 100, n + 1)
-    y = np.random.randint(0, 100, n + 1)
-    z = np.random.randint(0, 100, n + 1)
-
-    # Должны разделить точки на классы, применим для этого k-means
-    points = []
-
-    for i in range(n):
-        points.append([x[i], y[i], z[i]])
-
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(points)
-    clusters = kmeans.labels_
-
-    colors = ['red'] * n
-
-    for i in range(n):
-        if clusters[i] == 1:
-            colors[i] = 'blue'
-
-    # Добавление новой точки которую нужно будет предсказать
-    x_new = np.random.randint(0, 100)
-    y_new = np.random.randint(0, 100)
-    z_new = np.random.randint(0, 100)
-    points.append([x_new, y_new, z_new])
-    x[len(x) - 1] = x_new
-    y[len(y) - 1] = y_new
-    z[len(z) - 1] = z_new
-    colors.append('green')
-
-    weighss = train(points[:(len(points) - 1)], clusters, [1, 1, 1], 0.001, 10000)
-    log(weighss, points)
-
-    zz = lambda x, y: ((weighss[0] * x - weighss[1] * y) / weighss[2])
-
-    temp = np.linspace(0, 100, 50)
-    xx, yx = np.meshgrid(temp, temp)
-
-    show_figure(x, y, z, xx, yx, zz, colors)
